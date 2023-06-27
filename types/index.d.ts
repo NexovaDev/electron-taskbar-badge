b58c6d0123f94b5305e3265797b5ea9b9a2bd3aa
KK-Designs <KKart2007@gmail.com>
2023-06-26 21:23:34 -0700

    Fixed types for TS users

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,28 +1,41 @@
-declare module "electron-taskbar-badge" {
-	/**
-	 * @example const badgeOptions = {
-					fontColor: '#000000',
-					font: '62px Microsoft Yahei',
-					color: '#000000',
-					radius: 48,
-					updateBadgeEvent: 'notificationCount',
-					badgeDescription: 'Unread Notifications',
-					invokeType: 'handle',
-					max: 9,
-					fit: false,
-					useSystemAccentTheme: true,
-					additionalFunc: (count) => {
-						console.log(`Received ${count} new notifications!`);
-					},
-				};
+interface BadgeOptions {
+	fontColor: string,
+	font: string,
+	color: string,
+	radius: number,
+	updateBadgeEvent: string,
+	badgeDescription: string,
+	invokeType: string,
+	max: number,
+	fit: boolean,
+	useSystemAccentTheme: boolean,
+	additionalFunc(count: number): void
+}
 
-				new Badge(win, badgeOptions);
-	 * @since 1.0.0
-	 * @param {Electron.BrowserWindow} win
-	 * @param {object} badgeOptions
-	 * @returns {void}
-	*/
-	export default class Badge {
-		constructor(win: Electron.BrowserWindow, opts: { fontColor: string, font: string, color: string, radius: number, updateBadgeEvent: string, badgeDescription: string, invokeType: string, max: number, fit: boolean, useSystemAccentTheme: boolean, additionalFunc: Function })
-	}
+/**
+ * @example const badgeOptions = {
+				fontColor: '#000000',
+				font: '62px Microsoft Yahei',
+				color: '#000000',
+				radius: 48,
+				updateBadgeEvent: 'notificationCount',
+				badgeDescription: 'Unread Notifications',
+				invokeType: 'handle',
+				max: 9,
+				fit: false,
+				useSystemAccentTheme: true,
+				additionalFunc: (count) => {
+					console.log(`Received ${count} new notifications!`);
+				},
+			};
+
+			new Badge(win, badgeOptions);
+ * @since 1.0.0
+ * @param {BrowserWindow} win
+ * @param {object} opts
+ * @returns {void}
+*/
+
+export default class Badge {
+	constructor(win: object, opts: BadgeOptions)
 }
