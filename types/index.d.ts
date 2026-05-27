3d8cb3ad1c9290ddcb5e2df9ec0460e1b5efe142
onyx-nxt <kkart2007@gmail.com>
2026-05-27 14:21:55 -0700

    update type definitions for BadgeOptions

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,26 +1,24 @@
-interface BadgeOptions {
-	fontColor: string,
-	font: string,
+type BadgeType = 'alert' | 'attention' | 'error' | 'available' | 'away' | 'busy' | 'unavailable';
+
+export interface BadgeOptions {
+	fontColor?: string,
 	color: string,
-	radius: number,
 	updateBadgeEvent: string,
-	badgeDescription: string,
-	invokeType: string,
-	max: number,
-	fit: boolean,
-	useSystemAccentTheme: boolean,
-	onBadgeUpdate(count: number): void
+	badgeDescription?: string,
+	invokeType?: 'send' | 'handle',
+	max?: number,
+	useSystemAccentTheme?: boolean,
+	onBadgeUpdate?: (count: number | BadgeType | null) => void
 }
 
 /**
  * @example const badgeOptions = {
-				fontColor: '#000000',
+				fontColor: 'auto',
 				color: '#000000',
 				updateBadgeEvent: 'notificationCount',
 				badgeDescription: 'Unread Notifications',
 				invokeType: 'handle',
 				max: 9,
-				fit: false,
 				useSystemAccentTheme: true,
 				onBadgeUpdate: (count) => {
 					console.log(`Received ${count} new notifications!`);
@@ -29,11 +27,12 @@ interface BadgeOptions {
 
 			new Badge(win, badgeOptions);
  * @since 1.0.0
- * @param { BrowserWindow } win
- * @param { object } opts
+ * @param { import('electron').BrowserWindow } win
+ * @param { BadgeOptions } opts
  * @returns { void }
 */
 
 export default class Badge {
-	constructor(win: object, opts: BadgeOptions)
+	constructor(win: import('electron').BrowserWindow, opts: BadgeOptions)
+	update(badge: number | BadgeType | null): void
 }
